fb004d92f03dcb2315327ed397360eacf450180e
Aakash69420 <aakashbhattacharjee73@gmail.com>
2023-05-01 15:42:53 +0530

    login page connected

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -6,6 +6,8 @@ import ListItem from "../listItem/ListItem"
 export default function List({list}) {
  const[isMoved, setIsMoved] = useState(false)
  const[slideNumber, setslideNumber] = useState(0)
+ const[clickLimit, setClickLimit] = useState(window.innerWidth / 230)
+
 
  const listRef = useRef()
   console.log(list);
@@ -16,7 +18,7 @@ export default function List({list}) {
    setslideNumber(slideNumber - 1)
    listRef.current.style.transform = `translateX(${230 + distance}px)`
   }
-  if(direction === "right" && slideNumber < 4){
+  if(direction === "right" && slideNumber < 10 - clickLimit){
    setslideNumber(slideNumber + 1)
    listRef.current.style.transform = `translateX(${-230 + distance}px)`
   }
